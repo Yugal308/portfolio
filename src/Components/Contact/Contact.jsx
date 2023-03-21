96514fd92bc8cf92c7c86f08d7fe98b97497e946
Yugal308 <yverma_bemba19@thapar.edu>
2023-03-22 01:08:25 +0530

    project and contact done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import "./Contact.css"
-
+import {MdOutlineMail,MdWhatsapp} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
 const Contact = () => {
   return (
     <section id="contact">
-      
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineMail className='contact_icon' />
+            <h4>Email</h4>
+            <h5>yugal8200@gmail.com</h5>
+            <a href="mailto:yugal8200@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsLinkedin className='contact_icon'  />
+            <h4>Linkedin</h4>
+            <h5>Yugal Verma</h5>
+            <a target="_blank" href="https://www.linkedin.com/in/yugal-verma-28b3671ab/">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <MdWhatsapp className='contact_icon'  />
+            <h4>WhatsApp</h4>
+            <h5>+919899047003</h5>
+            <a target="_blank" href="https://api.whatsapp.com/send?phone=919899047003">Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder="Your Full Name" required />
+          <input type="text" name='email' placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Enter Your Message" required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }
--- src/Components/Project/Projects.jsx
+++ src/Components/Project/Projects.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
 import "./Project.css"
+import img1 from "../../Assets/portfolio1.jpg"
+import img2 from "../../Assets/portfolio2.jpg"
+import img3 from "../../Assets/portfolio3.jpg"
+import img4 from "../../Assets/portfolio4.jpg"
+import img5 from "../../Assets/portfolio5.png"
+import img6 from "../../Assets/portfolio6.jpg"
 const Projects = () => {
   return (
   <section id="projects">
-      
+      <h5>My Recent Projects </h5>
+      <h2>Portfolio</h2>
+        <div className="projects__container container">
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img1} alt="image_1" />
+            </div>
+            <h3>This is project title</h3>
+            <div className="project_link">
+            <a href="http://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img1} alt="image_1" />
+            </div>
+            <h3>This is project title</h3>
+            <div className="project_link">
+            <a href="http://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img1} alt="image_1" />
+            </div>
+            <h3>This is project title</h3>
+            <div className="project_link">
+            <a href="http://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img1} alt="image_1" />
+            </div>
+            <h3>This is project title</h3>
+            <div className="project_link">
+            <a href="http://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img1} alt="image_1" />
+            </div>
+            <h3>This is project title</h3>
+            <div className="project_link">
+            <a href="http://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img1} alt="image_1" />
+            </div>
+            <h3>This is project title</h3>
+            <div className="project_link">
+            <a href="http://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+        </div>
   </section>
   )
 }
