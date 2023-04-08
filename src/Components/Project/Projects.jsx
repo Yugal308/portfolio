8875387e973ad0a879a54709aeda8fcac78d4ae9
Yugal308 <yverma_bemba19@thapar.edu>
2023-04-08 18:23:18 +0530

    more projects added

--- src/Components/Project/Projects.jsx
+++ src/Components/Project/Projects.jsx
@@ -6,6 +6,9 @@ import img3 from "../../Assets/expense.jpeg"
 import img4 from "../../Assets/lost.jpeg"
 import img5 from "../../Assets/hulu.png"
 import img6 from "../../Assets/stock.jpeg"
+import img7 from "../../Assets/ecommerce.png"
+import img8 from "../../Assets/sales.jpg"
+import img9 from "../../Assets/hotel.jpg"
 const Projects = () => {
   return (
   <section id="projects">
@@ -69,6 +72,36 @@ const Projects = () => {
             <h3>Stock Price Predictor</h3>
             <div className="project_link">
             <a href="https://github.com/Yugal308/Stock_Price_Predictor" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img7} alt="image_7" />
+            </div>
+            <h3>React Ecommerce Site</h3>
+            <div className="project_link">
+            <a href="https://github.com/Yugal308/ecommerce" className='btn' target='_blank'>Github</a>
+            <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img8} alt="image_8" />
+            </div>
+            <h3>Sales Dashboard</h3>
+            <div className="project_link">
+            <a href="https://github.com/Yugal308" className='btn' target='_blank'>Github</a>
+            <a href="https://lookerstudio.google.com/reporting/4cdc51df-ed33-4f20-8771-e8c68c5a38cf" className='btn btn-primary' target='_blank'>Live Link</a>
+            </div>
+          </article>
+          <article className="project_item">
+            <div className="project_img">
+              <img src={img9} alt="image_9" />
+            </div>
+            <h3>Hotel Booking Data Analysis</h3>
+            <div className="project_link">
+            <a href="https://github.com/Yugal308/Hotel-Booking-Data-Analysis" className='btn' target='_blank'>Github</a>
             <a href="http://github.com/Yugal308" className='btn btn-primary' target='_blank'>Live Link</a>
             </div>
           </article>
